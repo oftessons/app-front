0bf584aece798a0d5a3fd17a18c7d31b21a3be43
DeniseRisia <risiaresponde@gmail.com>
2024-05-13 12:39:37 -0300

    teste prd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: false,
+  apiURLBase: 'http://localhost:8086',
+  //apiURLBase: 'https://back-app-dt.onrender.com',
+  clientId: 'oftlessons',
+  clientSecret: '@321',
+  obterTokenUrl: '/oauth/token'
 };
