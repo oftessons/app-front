043bb2663e0170aeeba816ba41101883919497cd
vctrwesley <vctrwesley@gmail.com>
2024-06-18 16:13:25 -0300

    primeira versao tela de cadastro

--- src/app/sistema/cadastro-questao/cadastro-questao.component.ts
+++ src/app/sistema/cadastro-questao/cadastro-questao.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro-questao.component.css']
 })
 export class CadastroQuestaoComponent implements OnInit {
+  options = [{ title: 'Opção 1', value: 'A', image: null }];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  addOption() {
+    const nextOptionValue = String.fromCharCode(this.options[this.options.length - 1].value.charCodeAt(0) + 1);
+    this.options.push({ title: `Opção ${this.options.length + 1}`, value: nextOptionValue, image: null });
+  }
+
+  removeOption(index: number) {
+    if (this.options.length > 1) {
+      this.options.splice(index, 1);
+
+      // Atualiza os valores
+      for (let i = 0; i < this.options.length; i++) {
+          this.options[i].value = String.fromCharCode(65 + i); // 65 é o código ASCII para 'A'
+      }
+  } else {
+      alert('Pelo menos uma alternativa.');
+  }
+}
+
+showImage(event: any, index: number) {
+  if (event.target.files && event.target.files[0]) {
+    const reader = new FileReader();
+    
+    reader.onload = (e: any) => {
+        this.options[index].image = e.target.result;
+    }
+
+    reader.readAsDataURL(event.target.files[0]);
+}
+}
+
+cadastrarQuestao() {
+}
+
+limparFormulario() {
+}
+
+confirmarCadastro() {
+}
 
 }
