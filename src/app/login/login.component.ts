6acbc4bbecd38a2913369a4b4dc4a459c56d6e2c
DeniseRisia <risiaresponde@gmail.com>
2024-07-17 21:33:51 -0300

    teste login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -57,34 +57,26 @@ export class LoginComponent {
     this.cadastrando = false;
   }
 
-  cadastrar() {
+  cadastrar(){
     const usuario: Usuario = new Usuario();
     usuario.username = this.username;
     usuario.password = this.password;
-    usuario.nome = this.nome;
     usuario.email = this.email;
-    usuario.telefone = this.telefone;
-    usuario.cidade = this.cidade;
-    usuario.estado = this.estado;
-    
-    this.authService.salvar(usuario).subscribe(
-      response => {
-        this.mensagemSucesso = 'Cadastro realizado com sucesso! Efetue o login.';
-        this.cadastrando = false;
-        this.username = '';
-        this.nome = '';
-        this.password = '';
-        this.email = '';
-        this.telefone = '';
-        this.cidade = '';
-        this.estado = '';
-        this.errors = [];
-      },
-      errorResponse => {
-        this.errors = errorResponse.error.errors;
-      }
-    );
+    this.authService
+        .salvar(usuario)
+        .subscribe( response => {
+            this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
+            this.cadastrando = false;
+            this.username = '';
+            this.password = '';
+            this.email = '';
+            this.errors = []
+        }, errorResponse => {
+            this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
+            this.errors = errorResponse.error.errors;
+        })
   }
+
 
   salvaUserLocal() {
     this.authService.obterUsuarioAutenticadoDoBackend().subscribe(
