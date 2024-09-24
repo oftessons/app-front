408c60c0660bba4857b9a09c027a8b0d3f39ef3e
DeniseRisia <risiaresponde@gmail.com>
2024-09-24 16:51:54 -0300

    ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
   clientId: 'oftlessons',
   clientSecret: '@321',
   obterTokenUrl: '/oauth/token',
-  // denise
+  // denise teste vercel
   tinymceApiKey: 'f6eb0n61ploi518j08cp3htisfrhn0yhyjfr2pk8l2bewri7',
 };
