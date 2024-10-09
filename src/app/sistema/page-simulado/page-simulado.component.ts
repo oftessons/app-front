061e2efbbaddb570b88c263bd042418d40ed9bb1
vctrwesley <vctrwesley@gmail.com>
2024-10-09 15:17:25 -0300

    subindo parte do select com filtro nos simulados junto da logica dos graficos

--- src/app/sistema/page-simulado/page-simulado.component.ts
+++ src/app/sistema/page-simulado/page-simulado.component.ts
@@ -101,6 +101,13 @@ export class PageSimuladoComponent implements OnInit {
 
   simuladoIniciado = false;
   simuladoFinalizado = false;
+  tempoTotal: number = 0;
+
+  tiposDeProvaDescricoes: string[] = [];
+  anosDescricoes: string[] = [];
+  dificuldadesDescricoes: string[] = [];
+  subtemasDescricoes: string[] = [];
+  temasDescricoes: string[] = [];
 
   constructor(
     private questoesService: QuestoesService,
@@ -114,6 +121,11 @@ export class PageSimuladoComponent implements OnInit {
     this.dados = this.obterDados();
     console.log('Dados:', this.dados);
     this.obterPerfilUsuario();
+    this.tiposDeProvaDescricoes = this.tiposDeProva.map(tipoDeProva => this.getDescricaoTipoDeProva(tipoDeProva));
+    this.anosDescricoes = this.anos.map(ano => this.getDescricaoAno(ano));
+    this.dificuldadesDescricoes = this.dificuldades.map(dificuldade => this.getDescricaoDificuldade(dificuldade));
+    this.subtemasDescricoes = this.subtemas.map(subtema => this.getDescricaoSubtema(subtema));
+    this.temasDescricoes = this.temas.map(tema => this.getDescricaoTema(tema));
   }
 
   finalizarSimulado() {
@@ -265,25 +277,54 @@ export class PageSimuladoComponent implements OnInit {
     const filtros: any = {};
 
     if (this.selectedAno) {
-      filtros.ano = this.selectedAno;
+      const anoSelecionado = this.anos.find(
+        (ano) => this.getDescricaoAno(ano) === this.selectedAno
+      );
+      if (anoSelecionado) {
+        filtros.ano = anoSelecionado;
+      }
     }
     if (this.selectedDificuldade) {
-      filtros.dificuldade = this.selectedDificuldade;
+      const dificuldadeSelecionada = this.dificuldades.find(
+        (dificuldade) =>
+          this.getDescricaoDificuldade(dificuldade) === this.selectedDificuldade
+      );
+      if (dificuldadeSelecionada) {
+        filtros.dificuldade = dificuldadeSelecionada;
+      }
     }
     if (this.selectedTipoDeProva) {
-      filtros.tipoDeProva = this.selectedTipoDeProva;
+      const tipoDeProvaSelecionado = this.tiposDeProva.find(
+        (tipoDeProva) =>
+          this.getDescricaoTipoDeProva(tipoDeProva) === this.selectedTipoDeProva
+      );
+      if (tipoDeProvaSelecionado) {
+        filtros.tipoDeProva = tipoDeProvaSelecionado;
+      }
     }
     if (this.selectedSubtema) {
-      filtros.subtema = this.selectedSubtema;
+      const subtemaSelecionado = this.subtemas.find(
+        (subtema) => this.getDescricaoSubtema(subtema) === this.selectedSubtema
+      );
+      if (subtemaSelecionado) {
+        filtros.subtema = subtemaSelecionado;
+      }
     }
     if (this.selectedTema) {
-      filtros.tema = this.selectedTema;
+      const temaSelecionado = this.temas.find(
+        (tema) => this.getDescricaoTema(tema) === this.selectedTema
+      );
+      if (temaSelecionado) {
+        filtros.tema = temaSelecionado;
+      }
     }
     if (this.palavraChave) {
       filtros.palavraChave = this.palavraChave;
     }
     if (this.quantidadeQuestoesSelecionada) {
       filtros.qtdQuestoes = this.quantidadeQuestoesSelecionada;
+    } else {
+      console.error('Quantidade de questões selecionada é inválida:', this.quantidadeQuestoesSelecionada);
     }
 
     if (Object.keys(filtros).length === 0) {
@@ -298,8 +339,23 @@ export class PageSimuladoComponent implements OnInit {
       .subscribe(
         (questoes: Questao[]) => {
           if (questoes.length === 0) {
-            this.message =
-              'Nenhuma questão encontrada para os filtros selecionados.';
+            if (filtros.ano && !filtros.tipoDeProva && !filtros.dificuldade && !filtros.subtema && !filtros.tema && !filtros.palavraChave && !filtros.qtdQuestoes) {
+              this.message = 'Nenhuma questão encontrada para o ano selecionado.';
+            } else if (filtros.tipoDeProva && !filtros.ano && !filtros.dificuldade && !filtros.subtema && !filtros.tema && !filtros.palavraChave && !filtros.qtdQuestoes) {
+              this.message = 'Nenhuma questão encontrada para o tipo de prova selecionado.';
+            } else if (filtros.dificuldade && !filtros.ano && !filtros.tipoDeProva && !filtros.subtema && !filtros.tema && !filtros.palavraChave && !filtros.qtdQuestoes) {
+              this.message = 'Nenhuma questão encontrada para a dificuldade selecionada.';
+            } else if (filtros.subtema && !filtros.ano && !filtros.tipoDeProva && !filtros.dificuldade && !filtros.tema && !filtros.palavraChave && !filtros.qtdQuestoes) {
+              this.message = 'Nenhuma questão encontrada para o subtema selecionado.';
+            } else if (filtros.tema && !filtros.ano && !filtros.tipoDeProva && !filtros.dificuldade && !filtros.subtema && !filtros.palavraChave && !filtros.qtdQuestoes) {
+              this.message = 'Nenhuma questão encontrada para o tema selecionado.';
+            } else if (filtros.palavraChave && !filtros.ano && !filtros.tipoDeProva && !filtros.dificuldade && !filtros.subtema && !filtros.tema && !filtros.qtdQuestoes) {
+              this.message = 'Nenhuma questão encontrada para a palavra-chave informada.';
+            } else if (filtros.qtdQuestoes && !filtros.ano && !filtros.tipoDeProva && !filtros.dificuldade && !filtros.subtema && !filtros.tema && !filtros.palavraChave) {
+              this.message = 'Nenhuma questão encontrada para a quantidade de questões selecionada.';
+            } else {
+              this.message = 'Nenhuma questão encontrada para os filtros selecionados.';
+            }
             this.questoes = [];
             this.idsQuestoes = [];
             this.questaoAtual = null;
