2eda518169e720d6ecd0ccc2dbb96a1f3dc78dfb
DeniseRisia <risiaresponde@gmail.com>
2024-10-15 12:19:51 -0300

    cores do quill ok, botoes responsivos do filtrar, videos nao estao aparecendo.

---
+++ src/app/pipes/safe-html.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'safeHtml'
+})
+export class SafeHtmlPipe implements PipeTransform {
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  transform(content: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(content);
+  }
+}
