6a7557b3e13f299e498cc366a1e0217e4618aa20
DeniseRisia <risiaresponde@gmail.com>
2024-10-27 10:21:16 -0300

    enum respon/nao

---
+++ src/app/sistema/page-questoes/enums/resp-simu-descricao.ts
@@ -0,0 +1,6 @@
+import { RespostasSimulado } from "./resp-simu";
+
+export const RespostasSimuladosDescricao: Record<RespostasSimulado, string> ={
+    [RespostasSimulado.RESPONDIDO]:'Respondido',
+    [RespostasSimulado.NAO_RESPONDIDO]:'Não Respondido',
+}
---
+++ src/app/sistema/page-questoes/enums/resp-simu.ts
@@ -0,0 +1,4 @@
+export enum RespostasSimulado {
+    RESPONDIDO = 'RESPONDIDO',
+    NAO_RESPONDIDO = 'NAO_RESPONDIDO'
+}
