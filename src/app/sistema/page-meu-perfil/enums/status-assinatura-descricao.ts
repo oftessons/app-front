963eb43d606410e6e832e1d32703d00ab674e50b
Guilherme Silva Sales <guilhermessales11@gmail.com>
2025-03-28 10:58:53 -0300

    stripe prod finish

--- src/app/sistema/page-meu-perfil/enums/status-assinatura-descricao.ts
+++ src/app/sistema/page-meu-perfil/enums/status-assinatura-descricao.ts
@@ -2,5 +2,6 @@ import { StatusAssinatura } from '../enums/status-assinatura';
 
 export const StatusAssinaturaDescricao: Record<StatusAssinatura, string> = {
     [StatusAssinatura.ATIVA]: 'Conta ativa',
+    [StatusAssinatura.TESTE]: 'Período de teste',
     [StatusAssinatura.CANCELADA]: 'Conta cancelada'
 }
