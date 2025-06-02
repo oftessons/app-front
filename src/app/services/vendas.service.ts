7c566a56f263efaa3b09feb0daca6a1dca29b315
Guilherme Sales <guilhermessales11@gmail.com>
2025-06-01 23:06:00 -0300

    feature: vendas hotmart e stripe

---
+++ src/app/services/vendas.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VendasService {
+  apiURL: string = environment.apiURLBase + '/api/vendas';
+
+  constructor(private http: HttpClient) {}
+
+
+  obterDadosAssinatura(): Observable<any>{ 
+    const url = `${this.apiURL}/obter-informacoes-plano`;
+
+    return this.http.get(url, {});
+
+  }
+
+  obterPortalAcesso(): Observable<any>{
+    const url = `${this.apiURL}/obter-portal-acesso`; 
+
+    return this.http.get(url, {});
+
+  }
+
+}
