06be6d50f619a490a18870ae0d57e6a2580ee0c2
Guilherme Sales <guilhermessales11@gmail.com>
2025-09-09 18:36:51 -0300

    fix: Resolvido filtro de questoes

--- src/app/sistema/page-questoes/enums/certas-erradas-descricao.ts
+++ src/app/sistema/page-questoes/enums/certas-erradas-descricao.ts
@@ -1,6 +1,6 @@
 import { CertasErradas } from "./certas-erradas";
 
 export const CertasErradasDescricao: Record<CertasErradas, string> = {
-    [CertasErradas.CERTA]: 'Certas',
-    [CertasErradas.ERRADA]: 'Erradas'
+    [CertasErradas.CERTA]: 'Certa',
+    [CertasErradas.ERRADA]: 'Errada'
 }
