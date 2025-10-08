e8b6459332ccf4d1b52ff5cd721cd10ccda08961
Caio Lemos <caioledan@gmail.com>
2025-10-08 11:29:29 -0300

    Cards Dinâmicos

--- src/app/shared/flashcards-temas-cards/flashcards-temas-cards.component.ts
+++ src/app/shared/flashcards-temas-cards/flashcards-temas-cards.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-flashcards-temas-cards',
   templateUrl: './flashcards-temas-cards.component.html',
   styleUrls: ['./flashcards-temas-cards.component.css']
 })
-export class FlashcardsTemasCardsComponent implements OnInit {
-
+export class FlashcardsTemasCardsComponent {
+  @Input() tema: string = 'Tema';
+  @Input() numeroFlashcards: number = 0;
+  @Input() caminho: string = '#';
+ 
   constructor() { }
 
   ngOnInit(): void {
