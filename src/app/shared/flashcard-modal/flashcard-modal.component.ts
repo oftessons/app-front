f5046e45a6e52fc68f23ec7aaedb9a67d7c93c45
Caio Lemos <caioledan@gmail.com>
2025-10-10 20:46:49 -0300

    Finalização dos cards, faltando apenas a animação

--- src/app/shared/flashcard-modal/flashcard-modal.component.ts
+++ src/app/shared/flashcard-modal/flashcard-modal.component.ts
@@ -1,14 +1,22 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+
+type FlashcardsState = 'question' | 'answer' | 'summary'
 
 @Component({
   selector: 'app-flashcard-modal',
   templateUrl: './flashcard-modal.component.html',
   styleUrls: ['./flashcard-modal.component.css']
 })
-export class FlashcardModalComponent implements OnInit {
+export class FlashcardModalComponent implements OnInit, OnChanges {
 
   @Input() isVisible = false;
   @Output() close = new EventEmitter<void>();
+
+  estado_atual: FlashcardsState = 'question';
+
+  question = 'Qual estrutura é responsável pela drenagem do humor aquoso?';
+  answer = 'Canal de Schlemm';
+  answerDescription =  "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
 
   constructor() { }
@@ -16,8 +24,27 @@ export class FlashcardModalComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isVisible']) {
+      
+      if (!changes['isVisible'].currentValue) {
+
+        this.estado_atual = 'question';
+      }
+    }
+  }
+
   onCloseClick(): void {
     this.close.emit();
+  }
+
+  showAnswer(): void {
+    this.estado_atual = 'answer';
+  }
+
+  //Quando o back ficar pronto, devo mudar essa função para receber a nota
+  rateAnswer(rating: number): void {
+    this.estado_atual = 'summary';
   }
 
 }
