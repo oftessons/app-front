910ce9f6fc0f0cc0d8f1196d76c62c4409d5f5bd
thiagovb62 <thiagovb62@gmail.com>
2025-11-07 18:19:45 -0300

    feat: adiciona novos estilos e estrutura ao componente Ofensiva com melhorias na exibição de dias

--- src/app/shared/ofensiva/ofensiva.component.ts
+++ src/app/shared/ofensiva/ofensiva.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfensivaComponent implements OnInit {
 
+ naOfensivaStyle = {
+  width: '20px',
+  height: '20px',
+  borderRadius: '50%',
+  border: '2px solid #28A745',
+  display: 'block',
+  boxSizing: 'border-box',
+  backgroundColor: '#28A745'
+};
+noOfensivaStyle = {
+  width: '20px',
+  height: '20px',
+  borderRadius: '50%',
+  border: '2px solid #8F97A6',
+  display: 'block',
+  boxSizing: 'border-box'
+};
+
+ checkMarkcontainer = {
+   display: 'flex',
+   justifyContent: 'center',
+   alignItems: 'center',
+   width: '100%',
+   height: '100%'
+ };
+
+  isSemanaFinish: boolean = false;
+  diasOfensiva: number = 0;
+  naOfensiva: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
