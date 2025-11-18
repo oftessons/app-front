c5f3235577f7eda0202e42217eaacdc806d4661c
Felipe Nogueira <felipe.leite23@gmail.com>
2025-11-18 17:24:23 -0300

    add modalSimulado outlet e melhoria da logica do canDeactivate com confirmacao via modal

--- src/app/services/CanDeactivateSimulado.Guard.ts
+++ src/app/services/CanDeactivateSimulado.Guard.ts
@@ -1,24 +1,37 @@
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
 import { PageSimuladoComponent } from '../sistema/page-simulado/page-simulado.component';
+import { ModalDeleteService } from './modal-delete.service';
 
 
 @Injectable({
     providedIn: 'root',
 })
 export class CanDeactivateSimuladoGuard implements CanDeactivate<PageSimuladoComponent> {
-    canDeactivate(component: PageSimuladoComponent): boolean {
-        if (component.isSimuladoIniciado && !component.simuladoFinalizado && !component.revisandoSimulado) {
-            const confirmar = confirm(
-                'Você está saindo do simulado. Seu progresso será salvo e o simulado será finalizado. Deseja continuar?'
-            );
+    constructor(private modalDeleteService: ModalDeleteService) {}
 
-            if (confirmar) {
-                component.finalizarSimulado();
-                return true; 
-            } 
+    canDeactivate(component: PageSimuladoComponent): Promise<boolean> | boolean {
+        if (component.isSimuladoIniciado && !component.simuladoFinalizado && !component.revisandoSimulado) {
+            return new Promise<boolean>((resolve) => {
+                this.modalDeleteService.openModal(
+                    {
+                        title: 'Sair do Simulado',
+                        description: 'Você está saindo do simulado. Seu progresso será salvo e o simulado será finalizado. Deseja continuar?',
+                        deletarTextoBotao: 'Sair',
+                        size: 'max-w-xl'
+                    },
+                    () => {
+                        // Confirmou a saída
+                        component.finalizarSimulado();
+                        resolve(true);
+                    },
+                    () => {
+                        // Cancelou
+                        resolve(false);
+                    }
+                );
+            });
         }
-        
-        return false; 
+        return true; 
     }
 }
