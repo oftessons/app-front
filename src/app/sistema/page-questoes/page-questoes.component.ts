830b8d60bc8083ceac451c936e354e2c061f3ac3
Guilherme Sales <guilhermessales11@gmail.com>
2025-12-13 15:25:43 -0300

    Chore Adicionado TimeStamp no RequestParam do service de questoes

--- src/app/sistema/page-questoes/page-questoes.component.ts
+++ src/app/sistema/page-questoes/page-questoes.component.ts
@@ -180,6 +180,7 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
   totalElementos: number = 0;
   listaIdsCarregados: number[] = [];
   seedAtual: number | null = null;
+  timestampBusca: number | null = null;
 
   filtros: any = {
     ano: null,
@@ -318,7 +319,7 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
     const estadoNavegacao = this.questoesService.obterEstadoNavegacao();
 
     if (estadoNavegacao && filtroStateJson) {
-      this.restaurarEstadoDoLocalStorage(filtroStateJson, true); 
+      this.restaurarEstadoDoLocalStorage(filtroStateJson, true);
     }
     else if (navigationState?.filtroId && navigationState.revisandoFiltro) {
       this.revisandoFiltroSalvo = true;
@@ -358,7 +359,7 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
     this.mostrarFiltroCacheado = false;
     const filtroState = JSON.parse(filtroStateJson);
 
-    this.seedAtual = filtroState.seed ?? null; 
+    this.seedAtual = filtroState.seed ?? null;
 
     if (!voltandoDaEdicao) {
       localStorage.removeItem('questoesFiltroState');
@@ -405,7 +406,7 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
     const filtros = this.construirObjetoFiltros();
 
     this.questoesService
-      .filtrarQuestoes(this.usuarioId, filtros, this.paginaAPI, this.itensPorPagina, this.seedAtual ?? undefined)
+      .filtrarQuestoes(this.usuarioId, filtros, this.timestampBusca, this.paginaAPI, this.itensPorPagina)
       .subscribe({
         next: (response: any) => {
           const novas = response.questoes.content || response.questoes;
@@ -1124,9 +1125,10 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
     this.paginaAPI = 0;
     this.listaDeIds = [];
     this.seedAtual = null;
+    this.timestampBusca = new Date().getTime();
 
     this.questoesService
-      .filtrarQuestoes(this.usuarioId, filtros, this.paginaAPI, this.itensPorPagina, this.seedAtual ?? undefined)
+      .filtrarQuestoes(this.usuarioId, filtros, this.timestampBusca, this.paginaAPI, this.itensPorPagina)
       .subscribe(
         (response: any) => {
           const novasQuestoes = response.questoes.content ? response.questoes.content : response;
@@ -1186,7 +1188,7 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
     this.carregando = true;
     this.paginaAPI++;
     this.questoesService
-      .filtrarQuestoes(this.usuarioId, filtros, this.paginaAPI, this.itensPorPagina, this.seedAtual ?? undefined)
+      .filtrarQuestoes(this.usuarioId, filtros, this.timestampBusca, this.paginaAPI, this.itensPorPagina)
       .subscribe(
         (response: any) => {
           const novas = response.questoes.content || [];
@@ -1256,7 +1258,7 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
     const paginaAnteriorParaBuscar = this.offsetPagina - 1;
 
     this.questoesService
-      .filtrarQuestoes(this.usuarioId, filtros, paginaAnteriorParaBuscar, this.itensPorPagina, this.seedAtual ?? undefined)
+      .filtrarQuestoes(this.usuarioId, filtros, this.timestampBusca, paginaAnteriorParaBuscar, this.itensPorPagina)
       .subscribe(
         (response: any) => {
           const novas = response.questoes.content || [];
@@ -1329,7 +1331,7 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
             return;
           }
           for (let i = 0; i < this.listaDeIds.length; i++) {
-            const questaoId = this.listaDeIds[i]; 
+            const questaoId = this.listaDeIds[i];
             if (!this.respostasSalvas.has(questaoId)) {
               proximaPaginaNaoRespondida = i;
               todasRespondidas = false;
@@ -1339,7 +1341,7 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
 
           if (todasRespondidas) {
             proximaPaginaNaoRespondida =
-              this.listaDeIds.length > 0 ? this.listaDeIds.length - 1 : 0; 
+              this.listaDeIds.length > 0 ? this.listaDeIds.length - 1 : 0;
           }
 
           this.paginaAtual = proximaPaginaNaoRespondida;
@@ -1432,11 +1434,11 @@ export class PageQuestoesComponent implements OnInit, AfterViewChecked {
     else {
       this.carregando = true;
       this.paginaAPI = paginaDesejadaAPI;
-      this.offsetPagina = paginaDesejadaAPI; 
+      this.offsetPagina = paginaDesejadaAPI;
 
       const filtros = this.construirObjetoFiltros();
 
-      this.questoesService.filtrarQuestoes(this.usuarioId, filtros, this.paginaAPI, this.itensPorPagina, this.seedAtual ?? undefined)
+      this.questoesService.filtrarQuestoes(this.usuarioId, filtros, this.timestampBusca, this.paginaAPI, this.itensPorPagina)
         .subscribe({
           next: (response: any) => {
             const novas = response.questoes.content || response;
