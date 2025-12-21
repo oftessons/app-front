d9f5ffe2acb985b1467a0f66b827bdb7a76e62d7
LucasRP <rodrigues.lrp21@gmail.com>
2025-12-21 17:30:41 -0300

    feat: add os vídeos editados

--- src/app/sistema/pagina-inicial/info_mock.ts
+++ src/app/sistema/pagina-inicial/info_mock.ts
@@ -210,32 +210,32 @@ export const PROFESSORESCOMENTADORES:Professor [] = [
 export const FEATURES = [
     {
       title: 'IA COM PROMPTS AUTORAIS',
-      video: '../../../assets/videos/trilha.mp4', 
+      video: '../../../assets/videos/landingpage/chatbot.mp4', 
       poster: '../../../assets/imagens/landingpage/desempenhopreview.jpg'
     },
     {
       title: 'SUGESTÃO DE REVISÃO POR IA',
-      video: '../../../assets/videos/trilha.mp4',
+      video: '../../../assets/videos/landingpage/mentoria.mp4', 
       poster: '../../../assets/imagens/landingpage/desempenhopreview.jpg'
     },
     {
       title: 'AULAS ILUSTRADAS E AUDIOVISUAL PROFISSIONAL',
-      video: '../../../assets/videos/trilha.mp4',
+      video: '../../../assets/videos/landingpage/aula.mp4', 
       poster: '../../../assets/imagens/landingpage/desempenhopreview.jpg'
     },
     {
       title: 'DASHBOARD DE MÉTRICAS DA SUA PROVA',
-      video: '../../../assets/videos/trilha.mp4',
+      video: '../../../assets/videos/landingpage/metricas.mp4', 
       poster: '../../../assets/imagens/landingpage/desempenhopreview.jpg'
     },
     {
       title: 'SIMULADOS COM DESEMPENHO',
-      video: '../../../assets/videos/trilha.mp4',
+      video: '../../../assets/videos/landingpage/simuladodesempenho.mp4', 
       poster: '../../../assets/imagens/landingpage/desempenhopreview.jpg'
     },
     {
       title: 'FILTRO INTELIGENTE DE QUESTÕES',
-      video: '../../../assets/videos/trilha.mp4',
+      video: '../../../assets/videos/landingpage/filtroQuestao.mp4', 
       poster: '../../../assets/imagens/landingpage/desempenhopreview.jpg'
     }
   ];
